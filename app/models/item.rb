d52bf94e58c69aedad39e67bbf2b9debcfcbae35
reddevilcero <reddevil_cero@hotmail.com>
2020-03-30 19:30:58 +0100

    basic relations between models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :category
+  has_many :order_items
+  has_many :order, through: :order_items
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :orders 
+  has_many :items, through: :orders
+   has_many :comments
+  # has_many :commented_items, through: :comments, foreing_key: 'item_id'
 end
