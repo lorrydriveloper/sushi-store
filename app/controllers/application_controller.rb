d4f00dd67d3b4a3f5304bd2e1530efc601b303e1
reddevilcero <reddevil_cero@hotmail.com>
2020-03-31 11:54:49 +0100

    applying basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :current_user
+
+  private
+
+  def current_user
+    @user = (User.find_by_id(session[:user_id]) || User.new)
+  end
+
+  def logged_in?
+    !!current_user.id
+  end
+
+  def require_logged_in
+    redirect_to login_path unless logged_in?
+  end
+
+  helper_method :current_user
 end
