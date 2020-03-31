fafc8493b70099097fc7fc1445be81464b1bf39f
reddevilcero <reddevil_cero@hotmail.com>
2020-03-31 08:31:28 +0100

    add before action to be more DRY

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,66 +1,60 @@
+# frozen_string_literal: true
+
 class Admin::CategoriesController < ApplicationController
-    
-    def index
-      @categories = Category.all
-    end
+  before_action :find_category, except: %i[index new]
 
-    def show
-      @category = Category.find(params[:id])
-      @items = @category.items
-    end
+  def index
+    @categories = Category.all
+  end
 
+  def show
+    @items = @category.items
+  end
 
+  def new
+    @category = Category.new
+    render 'new'
+  end
 
-    def new
-      @category = Category.new
+  def create
+    if @category.save
+      flash[:success] = 'Category successfully created'
+      redirect_to admin_categories_path
+    else
+      flash[:error] = 'Something went wrong'
       render 'new'
     end
+  end
 
-    def create
-      @category = Category.new(category_params)
-      if @category.save
-        flash[:success] = "Category successfully created"
-        redirect_to admin_categories_path
-      else
-        flash[:error] = "Something went wrong"
-        render 'new'
-      end
+  def edit; end
+
+  def update
+    if @category.update_attributes(category_params)
+      flash[:success] = 'Category was successfully updated'
+      redirect_to admin_categories_path
+    else
+      flash[:error] = 'Something went wrong'
+      render 'edit'
     end
+  end
 
-    def edit
-      @category = Category.find(params[:id])
+  def destroy
+    if @categoy.destroy
+      flash[:success] = 'Category was successfully deleted.'
+      redirect_to admin_categories_path
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to admin_dashboard_path
     end
-    
+  end
 
-    def update
-      @category = Category.find(params[:id])
-        if @category.update_attributes(category_params)
-          flash[:success] = "Category was successfully updated"
-          redirect_to admin_categories_path
-        else
-          flash[:error] = "Something went wrong"
-          render 'edit'
-        end
-    end
+  private
 
-    def destroy
-      @categoy = Category.find(params[:id])
-      if @categoy.destroy
-        flash[:success] = 'Category was successfully deleted.'
-        redirect_to admin_categories_path
-      else
-        flash[:error] = 'Something went wrong'
-        redirect_to admin_dashboard_path
-      end
-    end
-    
-    
-    
-    private
+  def category_params
+    params.require(:category).permit(:name, :icon)
+  end
 
-    def category_params
-      params.require(:category).permit(:name, :icon)
-    end
-  
-
+  def find_category
+    @category = Category.find(params[:id])
+  end
 end
