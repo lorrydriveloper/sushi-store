d71a5a50567a911d15339aa46c4ecbe87ee177b0
reddevilcero <reddevil_cero@hotmail.com>
2020-03-31 10:30:54 +0100

    add email and secure password to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  has_many :orders 
+  validates :email, presence: true, uniqueness: true
+  has_secure_password
+
+  has_many :orders
   has_many :items, through: :orders
-   has_many :comments
+  has_many :comments
   # has_many :commented_items, through: :comments, foreing_key: 'item_id'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_161532) do
+ActiveRecord::Schema.define(version: 2020_03_31_091931) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_161532) do
     t.string "avatar"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
   add_foreign_key "comments", "items"
