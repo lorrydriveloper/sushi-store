3f96424ef646913aa3a2ca449c564757237b9225
reddevilcero <reddevil_cero@hotmail.com>
2020-03-31 17:19:31 +0100

    add email validation

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
+gem 'email_address'
+
 # Omniauth Strategies
 
 gem 'omniauth'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,15 @@
 
 class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
+  validate :email_validator
   has_secure_password
 
   has_many :orders
   has_many :items, through: :orders
   has_many :comments
   # has_many :commented_items, through: :comments, foreing_key: 'item_id'
+
+  def email_validator
+    errors.add(:email, EmailAddress(email)) unless EmailAddress.valid? email
+  end
 end
