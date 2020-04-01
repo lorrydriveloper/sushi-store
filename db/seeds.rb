88ee4537c3b217805bc12ab45c431bb31d074c2c
reddevilcero <reddevil_cero@hotmail.com>
2020-04-01 10:06:30 +0100

    seeding the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,26 @@
-
-
-Category.destroy_all
+# frozen_string_literal: true
 Item.destroy_all
+Category.destroy_all
+
+
+Category.create(name: 'popular')
+Category.create(name: 'nigiri')
+Category.create(name: 'sashimi')
+Category.create(name: 'maki')
+Category.create(name: 'uramaki')
+Category.create(name: 'temaki')
+Category.create(name: 'offers')
+Category.create(name: 'sets')
+
+Category.all.each do |category|
+  15.times do
+    Item.create(
+      name: Faker::Food.sushi,
+      description: Faker::Food.description,
+      pieces: Faker::Number.within(range: 4..24),
+      price: Faker::Commerce.price(range: 2.99..19.99),
+      image: Faker::LoremFlickr.image(size: "650x325", search_terms: ['sushi']),
+      category: category
+    )
+  end
+end
