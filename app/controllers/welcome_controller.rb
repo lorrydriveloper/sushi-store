edc409839b1e99a4df2f7cc62632cb1803cfe8c8
reddevilcero <reddevil_cero@hotmail.com>
2020-04-01 10:17:08 +0100

    moving views to partials

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,11 +3,8 @@ class WelcomeController < ApplicationController
 
 
   def index
-
     @categories = Category.all 
-
+    @category = Category.find_by(name:'popular')
     @items = Item.all
-
-
   end
 end
