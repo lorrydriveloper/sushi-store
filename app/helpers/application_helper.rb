8afa11e8b33eef5517fa0c1ecd63d57b323d8d43
reddevilcero <reddevil_cero@hotmail.com>
2020-04-01 18:43:03 +0100

    moving to helpers small div

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def total_items
+    content_tag :div, class:'navigation__items' do
+      content_tag :p, session[:quantity]
+    end if session[:quantity]
+  end
 end
