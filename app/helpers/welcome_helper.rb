dd4a4fbeb45ff9bfc34165f03e73018541f9419d
reddevilcero <reddevil_cero@hotmail.com>
2020-04-02 17:09:55 +0100

    move some helpers

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,8 @@
-module WelcomeHelper
+module WelcomeHelper  
+  
+  def total_items
+    content_tag :div, class:'navigation__items' do
+      content_tag :p, session[:quantity]
+    end if session[:quantity]
+  end
 end
