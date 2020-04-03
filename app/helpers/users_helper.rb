244f117f95a8fb170890bc422e24eaed2df9db05
reddevilcero <reddevil_cero@hotmail.com>
2020-04-03 19:58:50 +0100

    chang vanilla Html to helpers

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,17 @@
+# frozen_string_literal: true
+
 module UsersHelper
+  def login_logout
+    if current_user.id
+      link_to('Logout', logout_path, class: 'navigation__link')
+    else
+       link_to('Login', login_path, class: 'navigation__link')
+    end
+  end
+
+  def user_account
+    content_tag :li, class:"navigation__item" do
+      link_to 'user account', user_path(current_user), class: 'navigation__link'
+    end if current_user.id
+  end
 end
