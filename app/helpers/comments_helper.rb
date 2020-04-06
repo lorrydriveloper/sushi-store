ff0a0557a6441bddb30131239d920281d5c45b46
reddevilcero <reddevil_cero@hotmail.com>
2020-04-06 11:24:00 +0100

    display buttons if commentbelong to user

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 module CommentsHelper
+  def crud_buttons(comment)
+    if belong_to_user?(comment)
+      link_to('edit', edit_item_comment_path(comment.item, comment)) +
+        button_to('delete', item_comment_path(comment.item, comment), method: :delete)
+    end
+  end
 end
