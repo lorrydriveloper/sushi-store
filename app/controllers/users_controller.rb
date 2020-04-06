322bfc98561874f551dd00d74df2e92524e4a329
reddevilcero <reddevil_cero@hotmail.com>
2020-04-06 16:25:40 +0100

    refactor to slim create logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,30 +2,14 @@
 
 class UsersController < ApplicationController
   before_action :require_logged_in, only: %i[edit update]
+
   def create
     @user = User.find_or_create_by(email: user_params[:email])
 
     if @user.valid?
-      if @user.try(:authenticate, user_params[:password])
-        session[:user_id] = @user.id
-        flash[:success] = "Welcome back #{@user.name}"
-        redirect_to root_path
-      else
-        flash[:error] = 'Password do not match try to login via Facebook'
-        redirect_to login_path
-      end
+      try_authenticate(@user)
     else
-      @user.password = user_params[:password]
-
-      if @user.save
-        flash[:success] = 'Welcome To Shushi to You please finish your profile.'
-        session[:user_id] = @user.id
-        redirect_to edit_user_path(@user)
-      else
-        flash[:error] = "#{@user.errors.full_messages.first} try again or Login with Social Media"
-        redirect_to login_path
-      end
-
+      try_create(@user)
     end
   end
 
@@ -48,5 +32,28 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :address, :avatar)
+  end
+
+  def try_authenticate(user)
+    if user.try(:authenticate, user_params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back #{user.name}"
+      redirect_to root_path
+    else
+      flash[:error] = 'Password do not match try to login via Facebook'
+      redirect_to login_path
+    end
+  end
+
+  def tyr_create(user)
+    user.password = user_params[:password]
+    if user.save
+      flash[:success] = 'Welcome To Sushi to You please finish your profile.'
+      session[:user_id] = user.id
+      redirect_to edit_user_path(user)
+    else
+      flash[:error] = "#{user.errors.full_messages.first} try again or Login with Social Media"
+      redirect_to login_path
+    end
   end
 end
