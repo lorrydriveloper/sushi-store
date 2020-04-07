6b06366631324d3e797f1750873cc52ef712a03f
reddevilcero <reddevil_cero@hotmail.com>
2020-04-07 15:37:14 +0100

    case insensitve for emails.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true, case_sensitive: false
   validate :email_validator
   has_secure_password
 
