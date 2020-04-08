48796bcf1377f8361376cdaf15ed457b5b7207e8
reddevilcero <reddevil_cero@hotmail.com>
2020-04-08 17:18:01 +0100

    improvment helper method

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -3,8 +3,16 @@
 module CommentsHelper
   def crud_buttons(comment)
     if belong_to_user?(comment)
-      link_to('edit', edit_item_comment_path(comment.item, comment)) +
-        button_to('delete', item_comment_path(comment.item, comment), method: :delete)
+      link_to('edit',
+              edit_item_comment_path(comment.item, comment),
+              class: 'btn btn-crud btn-crud--edit') +
+        button_to('delete', item_comment_path(comment.item, comment),
+                  class: 'btn btn-crud btn-crud--delete display-hack',
+                  method: :delete)
     end
+  end
+
+  def comment_by(comment)
+    "Commented by #{comment.user.name} #{time_ago_in_words(comment.created_at)} ago"
   end
 end
