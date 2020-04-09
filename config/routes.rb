fd7a857b43b6dd485027da0bdfcdf5ede0bb059a
reddevilcero <reddevil_cero@hotmail.com>
2020-04-09 16:40:08 +0100

    change auth route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'users#create'
-  get '/auth/facebook/callback' => 'sessions#create'
+  get '/auth/:provider/callback' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
   resources :orders do
