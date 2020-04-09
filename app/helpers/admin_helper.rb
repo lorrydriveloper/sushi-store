ec9ddb3429e83a1b9800e0bd9fb46377510aa2e9
reddevilcero <reddevil_cero@hotmail.com>
2020-04-09 19:29:46 +0100

    add helper to stadistics

---
+++ app/helpers/admin_helper.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module AdminHelper
+  def average_order(stats)
+    (stats[:profit] / stats[:orders]).round(2)
+  end
+
+  def order_diff(stats1, stats2)
+    (stats1[:orders] - stats2[:orders]) * (100 / stats2[:orders])
+  end
+
+  def value_diff(stats1, stats2)
+    today_average = average_order(stats1)
+    yesterday_average = average_order(stats2)
+    ((today_average - yesterday_average) * (100 / yesterday_average)).round(2)
+  end
+
+  def profit_diff(stats1, stats2)
+
+    (stats1[:profit] - stats2[:profit]).round(2)
+    
+  end
+  
+end
