29f9f4c9360fc900d2213b1a620261195d3d5817
reddevilcero <reddevil_cero@hotmail.com>
2020-04-10 10:30:04 +0100

    move buttons to helper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
 module CategoriesHelper
+  def category_crud_buttons(category)
+    content_tag :div, class: 'buttons' do
+      link_to('edit', edit_admin_category_path(category), 
+      class: ' btn btn-crud btn-crud--edit') +
+        link_to('show', admin_category_path(category), 
+        class: 'btn btn-crud btn-crud--show') +
+        button_to('destroy', admin_category_path(category), 
+        method: 'delete', class: 'btn btn-crud btn-crud--delete')
+    end
+  end
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,6 +1,17 @@
-module ItemsHelper
+# frozen_string_literal: true
 
+module ItemsHelper
   def items_in_cart(id)
     session[:cart][id.to_s] || '1'
+  end
+
+  def items_crud_buttons(item)
+    content_tag :div, class: 'buttons' do
+      link_to('edit', edit_admin_category_item_path(item.category, item),
+              class: 'btn btn-crud btn-crud--edit') +
+        link_to('show', item_path(item), class: 'btn btn-crud btn-crud--show') +
+        button_to('destroy', admin_category_item_path(item.category, item),
+                  class: 'btn btn-crud btn-crud--delete', method: :delete)
+    end
   end
 end
